7c17c81485793db01010403ba5d61a5839a89ff2
RKCP <rkcpeters@gmail.com>
2020-06-01 13:36:40 +0100

    Further testing on flutter. This time with Columns and Rows, and using them together on the app canvas.

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -9,8 +9,44 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey[850],
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                width: 100,
+                color: Colors.red,
+              ),
+              SizedBox(
+                width: 57,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 57,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
