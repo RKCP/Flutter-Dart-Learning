ddb0c35d8bfb2a96bd532f3fee5a1c1a1c9dc3e9
RKCP <rkcpeters@gmail.com>
2020-06-02 11:36:59 +0100

    Learning how to implement icons into the flutter app. Also trying to learn how to add custom icons and use them with flutter, as the given icons by the flutter team at google do not include all that I need.

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -14,26 +14,81 @@ class MyApp extends StatelessWidget {
             child: Column(
           children: <Widget>[
             CircleAvatar(
-              radius: 100,
+              radius: 75,
               backgroundImage: AssetImage('images/avatar.png'),
             ),
             Text(
               'Raphael Peters',
               style: TextStyle(
-                fontFamily: 'PermanentMarker',
-                fontSize: 40,
+                fontFamily: 'RockSalt',
+                fontSize: 30,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
-                letterSpacing: 4,
+                letterSpacing: 2,
               ),
             ),
             Text('SOFTWARE DEVELOPER',
                 style: TextStyle(
                   fontFamily: 'SourceSansPro',
-                  fontSize: 23,
-                  color: Colors.white,
+                  fontSize: 20,
+                  color: Colors.grey[100],
                   letterSpacing: 3,
-                ))
+                  fontWeight: FontWeight.bold,
+                )),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.grey[800],
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    "rkcpeters@gmail.com",
+                    style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                      color: Colors.grey[900],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 0, horizontal: 50),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(Icons.add_a_photo),
+                  Icon(Icons.add_alarm),
+                  Icon(Icons.add_alarm),
+                  Icon(Icons.add_alarm),
+                  Icon(Icons.add_alarm),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 0, horizontal: 50),
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Icon(Icons.add_a_photo),
+                  Icon(Icons.add_alarm),
+                  Icon(Icons.add_alarm),
+                  Icon(Icons.add_alarm),
+                  Icon(Icons.add_alarm),
+                ],
+              ),
+            )
           ],
         )),
       ),
