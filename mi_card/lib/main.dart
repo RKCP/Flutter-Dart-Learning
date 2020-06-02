d25e3abb606802f6d9c24f40a48b8762995343d2
RKCP <rkcpeters@gmail.com>
2020-06-02 13:26:59 +0100

    Business Card now has more information. Switched containers to Cards as they add shadow. Looking into switching row to ListTile next.

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:micard/custom_icons_icons.dart';
 
 void main() {
   runApp(MyApp());
@@ -35,60 +36,72 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 3,
                   fontWeight: FontWeight.bold,
                 )),
-            Container(
+            Card(
               color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 50, horizontal: 50),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.grey[800],
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Text(
-                    "rkcpeters@gmail.com",
-                    style: TextStyle(
-                      fontFamily: 'SourceSansPro',
-                      fontSize: 20,
-                      color: Colors.grey[900],
+              margin: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey[800],
                     ),
-                  )
-                ],
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'rkcpeters@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                        color: Colors.grey[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.fromLTRB(50, 0, 50, 30),
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(CustomIcons.github, color: Colors.grey[800]),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'https://github.com/RKCP',
+                      style: TextStyle(
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 20,
+                        color: Colors.grey[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
             Container(
-              color: Colors.white,
               margin: EdgeInsets.symmetric(vertical: 0, horizontal: 50),
               padding: EdgeInsets.all(10),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: <Widget>[
-                  Icon(Icons.add_a_photo),
-                  Icon(Icons.add_alarm),
-                  Icon(Icons.add_alarm),
-                  Icon(Icons.add_alarm),
-                  Icon(Icons.add_alarm),
+                  Icon(CustomIcons.java, color: Colors.white),
+                  Icon(CustomIcons.iconfinder_flutter_4691465,
+                      color: Colors.white),
+                  Icon(CustomIcons.sql_file_symbol, color: Colors.white),
+                  Icon(CustomIcons.html5, color: Colors.white),
+                  Icon(CustomIcons.css3_alt, color: Colors.white),
                 ],
               ),
             ),
-            Container(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 0, horizontal: 50),
-              padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Icon(Icons.add_a_photo),
-                  Icon(Icons.add_alarm),
-                  Icon(Icons.add_alarm),
-                  Icon(Icons.add_alarm),
-                  Icon(Icons.add_alarm),
-                ],
-              ),
-            )
           ],
         )),
       ),
