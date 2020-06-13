3552ec114aea5b16ff82924f832d4a554b52fef9
RKCP <rkcpeters@gmail.com>
2020-06-13 13:44:39 +0100

    Error not fixed, but added all the buttons and the sound works fine. Researching the solution to the callback error.

--- simple_xylophone/lib/main.dart
+++ simple_xylophone/lib/main.dart
@@ -10,17 +10,60 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              color: Colors.blue,
-              child: Text('hello'),
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note7.wav');
-              },
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    changeNote(1);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    changeNote(2);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.pink,
+                  onPressed: () {
+                    changeNote(3);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    changeNote(4);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    changeNote(5);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    changeNote(6);
+                  },
+                ),
+                FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    changeNote(7);
+                  },
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
   }
+}
+
+void changeNote(int buttonPressed) {
+  final player = AudioCache();
+  player.play('note$buttonPressed.wav');
 }
